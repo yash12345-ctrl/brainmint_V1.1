d6021eb3ca8ffbf6ad32eb3fae1d704fdfb64170
brainmint-debug <Devansh.official.01@gmail.com>
2026-03-06 14:49:40 +0530

    updated db config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-tn$9(ia8clau4)641(yde5)v1q8s82(4zz5i2mv%kps2_d8&w7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
 
@@ -126,3 +127,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- backend/engine/db.py
+++ backend/engine/db.py
@@ -1,10 +1,11 @@
 import pymysql
+import os
 
 def get_db():
     return pymysql.connect(
-        host="localhost",
-        user="django_user",
-        password="Django@123",
-        database="brainmint",
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER", "django_user"),
+        password=os.getenv("DB_PASSWORD", "Django@123"),
+        database=os.getenv("DB_NAME", "brainmint"),
         cursorclass=pymysql.cursors.DictCursor
     )
